2417c03a50afdd95af68e99f2048026dede275ca
penguinlover <qjavaswing2009@gmail.com>
2013-07-12 13:22:47 -0500

    redis url fix 2

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,6 @@
 if Rails.env.production?
   uri = URI.parse(ENV['REDISTOGO_URL'])
+  puts ENV['REDISTOGO_URL']
   $redis = Redis.new :host => uri.host, :port => uri.port, :password => uri.password  
 else
   $redis = Redis.new :host => 'localhost', :port => 6379
