a90058711f0eb8506c3fad43a5a7cea8d177b35e
penguinlover <qjavaswing2009@gmail.com>
2013-07-12 17:05:42 -0500

    make resque know where to find redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,9 @@
 if Rails.env.production?
   uri = URI.parse(ENV['REDISTOGO_URL'])
-  REDIS = Redis.new :host => uri.host, :port => uri.port, :password => uri.password
+  REDIS = Redis.new :host => uri.host, :port => uri.port, :password => uri.password  
 else
   REDIS = Redis.new :host => 'localhost', :port => 6379  
 end
+
+# config resque
+Resque.redis = REDIS
